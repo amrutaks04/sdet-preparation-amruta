607ab1729ec9dd9254ad523096a0438946ac47b3
amruta.ks <amruta.ks2022cse@sece.ac.in>
2025-07-26 14:28:53 +0530

    Inital commit

---
+++ verify.py
@@ -0,0 +1,12 @@
+import pytest
+import selenium
+import requests
+import pandas
+import openpyxl
+
+print("Package versions:")
+print("pytest version:",pytest.__version__)
+print("selenium version:", selenium.__version__)
+print("requests version:", requests.__version__)
+print("pandas version:", pandas.__version__)
+print("openpyxl version:", openpyxl.__version__)
